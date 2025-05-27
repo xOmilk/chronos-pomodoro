6bafad5e57122007addeb142ea10ef7df6e10e61
xOmilk <leite46052@gmail.com>
2025-05-27 15:19:35 -0300

    Container component

---
+++ src/components/Container/index.tsx
@@ -0,0 +1,15 @@
+import styles from "./styles.module.css";
+
+type ContainerProps = {
+	children: React.ReactNode;
+};
+
+export function Container({ children }: ContainerProps) {
+	return (
+		<div className={styles.container}>
+			<div className={styles.content}>
+				<section>{children}</section>
+			</div>
+		</div>
+	);
+}
