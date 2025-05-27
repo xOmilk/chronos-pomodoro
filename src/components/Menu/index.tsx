6361f05f187ecb9d51ac7b62515908cd2a7984ab
xOmilk <leite46052@gmail.com>
2025-05-27 15:18:53 -0300

    Componente Menu

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,22 @@
+import { History, House, Settings, Sun } from "lucide-react";
+import styles from "./styles.module.css";
+
+export function Menu() {
+	return (
+		<div className={styles.menu}>
+			<a className={styles.menuLink} href="#">
+				<House />
+			</a>
+
+			<a className={styles.menuLink} href="#">
+				<History />
+			</a>
+			<a className={styles.menuLink} href="#">
+				<Settings />
+			</a>
+			<a className={styles.menuLink} href="#">
+				<Sun />
+			</a>
+		</div>
+	);
+}
