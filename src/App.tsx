93c754177ab7aaca3862452de46a64a34d288767
xOmilk <leite46052@gmail.com>
2025-05-27 15:20:00 -0300

    Criação da base

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import "./App.css";
-import "./components/Heading";
+import "./css/theme.css";
+import "./css/global.css";
+
 import { Heading } from "./components/Heading";
+import { Container } from "./components/Container";
+import { Logo } from "./components/Logo";
+import { Menu } from "./components/Menu";
 
 export function App() {
 	return (
 		<>
-			<Heading />
+			<Container>
+				<Logo />
+			</Container>
+
+			<Container>
+				<Heading>MENU</Heading>
+
+				<Menu />
+			</Container>
 		</>
 	);
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
 import { App } from "./App";
 
+import "./css/theme.css";
+import "./css/global.css";
+
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App></App>
-  </StrictMode>
+	<StrictMode>
+		<App></App>
+	</StrictMode>
 );
