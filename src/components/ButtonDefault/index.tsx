e9329aab3a471bf386dd78d480100a9954d02832
xOmilk <leite46052@gmail.com>
2025-05-27 22:36:52 -0300

    Botao Default

---
+++ src/components/ButtonDefault/index.tsx
@@ -0,0 +1,26 @@
+import styles from "./styles.module.css";
+
+type ButtonDefaultProps = {
+	icon: React.ReactNode;
+	color?: "green" | "red";
+	idButtonElement: string;
+} & React.ComponentProps<"button">;
+
+export function ButtonDefault({
+	idButtonElement,
+	icon,
+	color = "green",
+	...props
+}: ButtonDefaultProps) {
+	return (
+		<>
+			<button
+				id={idButtonElement}
+				className={`${styles.button} ${styles[color]}`}
+				{...props}
+			>
+				{icon}
+			</button>
+		</>
+	);
+}
