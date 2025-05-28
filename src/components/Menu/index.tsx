91bfc6273365d6444037f59a178b13b1875f4001
xOmilk <leite46052@gmail.com>
2025-05-28 20:35:04 -0300

    Logica para troca de tema dentro do componente menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,21 +1,57 @@
-import { History, House, Settings, Sun } from "lucide-react";
+import { History, House, MoonIcon, Settings, SunIcon } from "lucide-react";
 import styles from "./styles.module.css";
+import { useEffect, useState } from "react";
+
+type AvailableThemes = "dark" | "light";
 
 export function Menu() {
+	const [theme, setTheme] = useState<AvailableThemes>(() => {
+		const storageTheme =
+			(localStorage.getItem("theme") as AvailableThemes) ?? "dark";
+		return storageTheme;
+	});
+
+	function handleThemeChange(
+		event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+	) {
+		event.preventDefault();
+
+		setTheme((prevTheme) => {
+			const nextTheme = prevTheme === "dark" ? "light" : "dark";
+			return nextTheme;
+		});
+	}
+
+	useEffect(() => {
+		//alterando o elemento principal
+		document.documentElement.setAttribute("data-theme", theme);
+		localStorage.setItem("theme", theme);
+	}, [theme]);
+
+	const nextThemeIcon = {
+		dark: <SunIcon />,
+		light: <MoonIcon />,
+	};
+
 	return (
 		<nav className={styles.menu}>
-			<a className={styles.menuLink} href="#">
+			<a title="Ir para a home" className={styles.menuLink} href="#">
 				<House />
 			</a>
 
-			<a className={styles.menuLink} href="#">
+			<a title="Verificar histórico" className={styles.menuLink} href="#">
 				<History />
 			</a>
-			<a className={styles.menuLink} href="#">
+			<a title="Ir para as configurações" className={styles.menuLink} href="#">
 				<Settings />
 			</a>
-			<a className={styles.menuLink} href="#">
-				<Sun />
+			<a
+				title="Mudar o tema"
+				className={styles.menuLink}
+				href="#"
+				onClick={handleThemeChange}
+			>
+				{nextThemeIcon[theme]}
 			</a>
 		</nav>
 	);
