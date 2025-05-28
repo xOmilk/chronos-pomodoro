a79f8d07b1581c849706a0cc5d80094c2764a6d2
xOmilk <leite46052@gmail.com>
2025-05-28 20:36:05 -0300

    Mudança esquematização + adição de componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./css/theme.css";
 import "./css/global.css";
 
-import { Heading } from "./components/Heading";
+//import { Heading } from "./components/Heading";
 import { Container } from "./components/Container";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
+import { CountDown } from "./components/CountDown";
+import { Footer } from "./components/Footer";
+import { FormMain } from "./components/FormMain";
 
 export function App() {
 	return (
@@ -14,9 +17,19 @@ export function App() {
 			</Container>
 
 			<Container>
-				<Heading>MENU</Heading>
-
 				<Menu />
+			</Container>
+
+			<Container>
+				<CountDown />
+			</Container>
+
+			<Container>
+				<FormMain></FormMain>
+			</Container>
+
+			<Container>
+				<Footer></Footer>
 			</Container>
 		</>
 	);
