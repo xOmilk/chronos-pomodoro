8da800706c666b9f8ada70c3a6a6b67f01e99f3e
xOmilk <leite46052@gmail.com>
2025-06-01 21:56:21 -0300

    Input controlado no React com "value", "onChange" e useState

--- src/components/FormMain/index.tsx
+++ src/components/FormMain/index.tsx
@@ -4,16 +4,27 @@ import { Cycles } from "../Cycles";
 import { PlayCircleIcon } from "lucide-react";
 
 import styles from "./styles.module.css";
+import { useState } from "react";
 
 export function FormMain() {
+	const [taskName, setTaskName] = useState("");
+
+	function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
+		event.preventDefault();
+		console.log("Lidando com a criação de task");
+		console.log("Tarefa", taskName);
+	}
+
 	return (
-		<div className={styles.form}>
+		<form onSubmit={handleCreateNewTask} className={styles.form}>
 			<div className={styles.formRow}>
 				<InputDefault
 					idInputElement="taskId"
 					labelText="TESTEEEE"
 					type="search"
 					placeholder="Digite algo"
+					value={taskName}
+					onChange={(e) => setTaskName(e.target.value)}
 				></InputDefault>
 			</div>
 			<div className={styles.formRow}>
@@ -28,6 +39,6 @@ export function FormMain() {
 					icon={<PlayCircleIcon />}
 				></ButtonDefault>
 			</div>
-		</div>
+		</form>
 	);
 }
