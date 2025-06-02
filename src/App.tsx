6b2c416860dc07a8a862ac728da2a35ac6eef4b9
xOmilk <leite46052@gmail.com>
2025-06-01 21:14:58 -0300

    Utilização de Contexto para as tasks

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,12 @@ import { Home } from "./pages/Home";
 
 import "./css/theme.css";
 import "./css/global.css";
+import { TaskContextProvider } from "./contexts/TaskContext/TaskContextProvider";
 
 export function App() {
-	return <Home />;
+	return (
+		<TaskContextProvider>
+			<Home />
+		</TaskContextProvider>
+	);
 }
--- src/components/CountDown/index.tsx
+++ src/components/CountDown/index.tsx
@@ -1,9 +1,14 @@
 import styles from "./styles.module.css";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
 
 export function CountDown() {
+	const { state } = useTaskContext();
+
 	return (
 		<div className={styles.countDown}>
-			<div className={styles.number}>00:00</div>
+			<div className={styles.number}>
+				{state.formattedSecondsRemaining}
+			</div>
 		</div>
 	);
 }
