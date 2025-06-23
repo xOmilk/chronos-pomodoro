cac21ca1004f63a7ddc131e3248544cdccc08ff4
xOmilk <leite46052@gmail.com>
2025-06-23 04:42:03 -0300

    implementacao de COUNT_DOWN && COMPLETE_TASK functions dentro do reducer

--- src/contexts/TaskContext/taskReducer.ts
+++ src/contexts/TaskContext/taskReducer.ts
@@ -42,6 +42,29 @@ export function taskReducer(
 		case TaskActionTypes.RESET_STATE: {
 			return state;
 		}
+		case TaskActionTypes.COUNT_DOWN: {
+			return {
+				...state,
+				secondsRemaining: action.payload.secondsRemaining,
+				formattedSecondsRemaining: formatSecondsToMinutes(
+					action.payload.secondsRemaining
+				),
+			};
+		}
+		case TaskActionTypes.COMPLETE_TASK: {
+			return {
+				...state,
+				activeTask: null,
+				secondsRemaining: 0,
+				formattedSecondsRemaining: "00:00",
+				tasks: state.tasks.map((task) => {
+					if (state.activeTask && state.activeTask.id === task.id) {
+						return { ...task, completeDate: Date.now() };
+					}
+					return task;
+				}),
+			};
+		}
 	}
 
 	return state;
