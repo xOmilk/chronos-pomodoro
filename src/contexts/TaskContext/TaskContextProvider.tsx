2ac52cd6414959a66fe6da6154e9a47fb8168b07
xOmilk <leite46052@gmail.com>
2025-06-23 04:44:18 -0300

    Implementação de função para o controle do countdown das tasks

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -2,6 +2,8 @@ import { useEffect, useReducer } from "react";
 import { initialTaskState } from "./initialTaskState";
 import { TaskContext } from "./TaskContext";
 import { taskReducer } from "./taskReducer";
+import { TimerWorkerManager } from "../../workers/TimerWorkerManager";
+import { TaskActionTypes } from "./taskActions";
 
 type TaskContextProviderProps = {
 	children: React.ReactNode;
@@ -10,9 +12,35 @@ type TaskContextProviderProps = {
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
 	const [state, dispatch] = useReducer(taskReducer, initialTaskState);
 
+	const worker = TimerWorkerManager.getInstance();
+
+	worker.onmessage((event) => {
+		const countDownSeconds = event.data;
+		console.log(countDownSeconds);
+
+		if (countDownSeconds <= 0) {
+			dispatch({
+				type: TaskActionTypes.COMPLETE_TASK,
+			});
+			console.log("Worker completed!!!");
+			worker.terminate();
+		} else {
+			dispatch({
+				type: TaskActionTypes.COUNT_DOWN,
+				payload: { secondsRemaining: countDownSeconds },
+			});
+		}
+	});
+
 	useEffect(() => {
 		console.log(state);
-	}, [state]);
+
+		if (!state.activeTask) {
+			worker.terminate();
+		}
+
+		worker.postMessage(state);
+	}, [worker, state]);
 
 	return (
 		<TaskContext.Provider value={{ state, dispatch }}>
