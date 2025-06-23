e5189c31e6d6b9692b25c4484ef63bad8c0f0e77
xOmilk <leite46052@gmail.com>
2025-06-23 04:40:56 -0300

    Adicao de novos tipos de task e separação por payload

--- src/contexts/TaskContext/taskActions.ts
+++ src/contexts/TaskContext/taskActions.ts
@@ -11,16 +11,31 @@ export enum TaskActionTypes {
 	START_TASK = "START_TASK",
 	INTERRUPT_TASK = "INTERRUPT_TASK",
 	RESET_STATE = "RESET_STATE",
+	COUNT_DOWN = "COUNT_DOWN",
+	COMPLETE_TASK = "COMPLETE_TASK",
 }
 
-export type TaskActionModel =
+export type TaskActionsWithPayload =
 	| {
 			type: TaskActionTypes.START_TASK;
 			payload: TaskModel;
 	  }
 	| {
+			type: TaskActionTypes.COUNT_DOWN;
+			payload: { secondsRemaining: number };
+	  };
+
+export type TaskActionsWithoutPayload =
+	| {
+			type: TaskActionTypes.RESET_STATE;
+	  }
+	| {
 			type: TaskActionTypes.INTERRUPT_TASK;
 	  }
 	| {
-			type: TaskActionTypes.RESET_STATE;
+			type: TaskActionTypes.COMPLETE_TASK;
 	  };
+
+export type TaskActionModel =
+	| TaskActionsWithPayload
+	| TaskActionsWithoutPayload;
