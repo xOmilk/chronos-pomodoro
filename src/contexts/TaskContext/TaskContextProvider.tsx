11dcb2468cc69121f3b9a0838d47d2cb2e3b88a9
xOmilk <leite46052@gmail.com>
2025-06-24 18:06:08 -0300

    Utilização do localStorage para recuperar um estado e setar esse novo estado a cada mudança

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -5,13 +5,26 @@ import { taskReducer } from "./taskReducer";
 import { TimerWorkerManager } from "../../workers/TimerWorkerManager";
 import { TaskActionTypes } from "./taskActions";
 import { loadBeep } from "../../utils/loadBeep";
+import type { TaskStateModel } from "../../models/TaskStateModel";
 
 type TaskContextProviderProps = {
 	children: React.ReactNode;
 };
 
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
-	const [state, dispatch] = useReducer(taskReducer, initialTaskState);
+	const [state, dispatch] = useReducer(taskReducer, initialTaskState, () => {
+		const storageState = localStorage.getItem("state");
+
+		if (storageState === null) return initialTaskState;
+
+		const parsedState = JSON.parse(storageState) as TaskStateModel;
+		return {
+			...parsedState,
+			activeTask: null,
+			secondsRemaining: 0,
+			formattedSecondsRemaining: "00:00",
+		};
+	});
 	const playBeepRef = useRef<ReturnType<typeof loadBeep> | null>(null);
 
 	const worker = TimerWorkerManager.getInstance();
@@ -37,6 +50,9 @@ export function TaskContextProvider({ children }: TaskContextProviderProps) {
 	});
 
 	useEffect(() => {
+		//Atualizar e armazenar o state
+		localStorage.setItem("state", JSON.stringify(state));
+
 		if (!state.activeTask) {
 			worker.terminate();
 		}
