cbd1eee69071d7f94266d0225f2a792b50bcafff
xOmilk <leite46052@gmail.com>
2025-07-12 00:16:09 -0300

    Adiciona componente de diálogo para confirmação de ações e integra com o adaptador de toast

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,70 +1,69 @@
+import { useEffect, useState } from "react";
 import { TrashIcon } from "lucide-react";
 import { ButtonDefault } from "../../components/ButtonDefault";
 import { Heading } from "../../components/Heading";
 import { MainTemplate } from "../../templates/MainTemplate";
 import { Container } from "../../components/Container";
+import { PrintTable } from "./PrintTable";
+
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { TaskActionTypes } from "../../contexts/TaskContext/taskActions";
+
+import { toast } from "react-toastify";
+import { showMessage } from "../../adapters/toastifyAdapter";
 
 import styles from "./styles.module.css";
-import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
-import { formatDate } from "../../utils/formatDate";
-import { getTaskStatus } from "../../utils/getTaskStatus";
+
+History.Table = PrintTable;
 
 export function History() {
-	const { state } = useTaskContext();
+	const [confirmClearHistory, setConfirmClearHistory] = useState(false);
+	const { state, dispatch } = useTaskContext();
+	const hasTasks = state.tasks.length > 0;
+
+	function handleResetHistory() {
+		toast.dismiss();
+		showMessage.confirm(
+			"Tem certeza que deseja apagar todo o historico?",
+			(confirmation) => {
+				setConfirmClearHistory(confirmation);
+			}
+		);
+	}
+
+	useEffect(() => {
+		if (!confirmClearHistory) return;
+
+		setConfirmClearHistory(false);
+
+		dispatch({ type: TaskActionTypes.RESET_STATE });
+	}, [confirmClearHistory, dispatch]);
+
+	useEffect(() => {
+		return () => {
+			showMessage.dismiss();
+		};
+	}, []);
 
 	return (
 		<MainTemplate>
 			<Heading>
 				<span>History</span>
 				<span className={styles.buttonContainer}>
-					<ButtonDefault
-						icon={<TrashIcon />}
-						color="red"
-						aria-label="Apagar todo o historico"
-						title="Apagar todo o historico"
-					/>
+					{hasTasks && (
+						<ButtonDefault
+							icon={<TrashIcon />}
+							color="red"
+							aria-label="Apagar todo o historico"
+							title="Apagar todo o historico"
+							onClick={handleResetHistory}
+						/>
+					)}
 				</span>
 			</Heading>
 
 			<Container>
-				<div className={styles.responsiveTable}>
-					<table>
-						<thead>
-							<tr>
-								<th>Tarefa</th>
-								<th>Duração</th>
-								<th>Data</th>
-								<th>Status</th>
-								<th>Tipo</th>
-							</tr>
-						</thead>
-
-						<tbody>
-							{state.tasks.map((task) => {
-								const taskTypeDictionary = {
-									workTime: "Foco",
-									longBreakTime: "Descanso longo",
-									shortBreakTime: "Descanso curto",
-								};
-
-								return (
-									<tr key={task.id}>
-										<td>{task.name}</td>
-										<td>{task.duration}</td>
-										<td>{formatDate(task.startDate)}</td>
-										<td>
-											{getTaskStatus(
-												task,
-												state.activeTask
-											)}
-										</td>
-										<td>{taskTypeDictionary[task.type]}</td>
-									</tr>
-								);
-							})}
-						</tbody>
-					</table>
-				</div>
+				<History.Table hasTasks={hasTasks} />
 			</Container>
 		</MainTemplate>
 	);
