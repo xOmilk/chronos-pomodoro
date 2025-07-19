169f8201add153d962b9b6cd9d7e1d57be31d3d4
xOmilk <leite46052@gmail.com>
2025-07-18 22:45:28 -0300

    Atualiza os tempos de trabalho e intervalos no estado inicial das tarefas

--- src/contexts/TaskContext/initialTaskState.ts
+++ src/contexts/TaskContext/initialTaskState.ts
@@ -6,8 +6,8 @@ export const initialTaskState: TaskStateModel = {
 	activeTask: null,
 	currentCycle: 0,
 	config: {
-		workTime: 1,
-		shortBreakTime: 1,
-		longBreakTime: 1,
+		workTime: 25,
+		shortBreakTime: 5,
+		longBreakTime: 15,
 	},
 };
